ae0773e4410282f8c517ead0797dbdbca1a0e35c
BETONCUM <sematrollrus@gmail.com>
2023-06-11 21:23:06 +1000

    fg

--- hw3/task03.py
+++ hw3/task03.py
@@ -19,6 +19,25 @@ class Filter:
 # positive_even.apply(range(100)) should return only even numbers from 0 to 99
 
 
+class Filter:
+    """
+        Helper filter class. Accepts a list of single-argument
+        functions that return True if object in list conforms to some criteria
+    """
+    def __init__(self, functions):
+        self.functions = functions
+
+    def apply(self, data):
+        return [
+            item for item in data 
+            if all(i(item) for i in self.functions)
+        ]
+
+# example of usage:
+# positive_even = Filter(lamba a: a % 2 == 0, lambda a: a > 0, lambda a: isinstance(int, a)))
+# positive_even.apply(range(100)) should return only even numbers from 0 to 99
+
+
 def make_filter(**keywords):
     """
         Generate filter object for specified keywords
--- hw3/task04.py
+++ hw3/task04.py
@@ -16,8 +16,25 @@ Write a function that detects if a number is Armstrong number in functionaly sty
 
 ### Example function signature and call
 """
+from functools import reduce
+
 def is_armstrong(number: int) -> bool:
-    ...
+    # Convert the number to a string to iterate over its digits
+    digits = list(str(number))
+    
+    # Calculate the power to raise each digit to
+    power = len(digits)
+    
+    # Use map to apply the power operation to each digit
+    powered_digits = map(lambda x: int(x) ** power, digits)
+    
+    # Use reduce to sum up all the powered digits
+    sum_of_digits = reduce(lambda x, y: x + y, powered_digits)
+    
+    # Check if the sum of digits is equal to the original number
+    return sum_of_digits == number
+
+# Test cases
   
 assert is_armstrong(153) == True, 'Is Armstrong number'
 assert is_armstrong(10) == False, 'Is not Armstrong number'
