9fea14198cec13aafe65b5b70937d0cc12afb6c4
BETONCUM <sematrollrus@gmail.com>
2023-06-11 22:01:05 +1000

    g

--- hw5/oop_2.py
+++ hw5/oop_2.py
@@ -52,6 +52,65 @@ PEP8 соблюдать строго.
 import datetime
 from collections import defaultdict
 
+class Homework:
+    def __init__(self, text: str, deadline: int):
+        self.text = text
+        self.deadline = datetime.timedelta(days=deadline)
+        self.created = datetime.datetime.now()
+
+    def is_active(self):
+        current_time = datetime.datetime.now()
+        return current_time <= (self.created + self.deadline)
+
+
+class Student:
+    def __init__(self, first_name: str, last_name: str):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def do_homework(self, homework: Homework, solution: str):
+        if not isinstance(homework, Homework):
+            raise TypeError('You gave a not Homework object')
+        if not homework.is_active():
+            raise DeadlineError('You are late')
+        return HomeworkResult(self, homework, solution)
+
+
+class Teacher:
+    homework_done = defaultdict(set)
+
+    def __init__(self, first_name: str, last_name: str):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def create_homework(self, text: str, deadline: int):
+        return Homework(text, deadline)
+
+    def check_homework(self, homework_result: 'HomeworkResult'):
+        if len(homework_result.solution) > 5:
+            self.homework_done[homework_result.homework].add(homework_result)
+
+    @classmethod
+    def reset_results(cls, homework=None):
+        if homework:
+            del cls.homework_done[homework]
+        else:
+            cls.homework_done.clear()
+
+
+class HomeworkResult:
+    def __init__(self, author: Student, homework: Homework, solution: str):
+        self.author = author
+        self.homework = homework
+        self.solution = solution
+        self.created = datetime.datetime.now()
+
+
+class DeadlineError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
 
 if __name__ == '__main__':
     opp_teacher = Teacher('Daniil', 'Shadrin')
@@ -65,20 +124,26 @@ if __name__ == '__main__':
 
     result_1 = good_student.do_homework(oop_hw, 'I have done this hw')
     result_2 = good_student.do_homework(docs_hw, 'I have done this hw too')
-    result_3 = lazy_student.do_homework(docs_hw, 'done')
+    try:
+        result_3 = lazy_student.do_homework(docs_hw, 'done')
+    except DeadlineError as e:
+        print(e)  # You are late
+
     try:
         result_4 = HomeworkResult(good_student, "fff", "Solution")
-    except Exception:
-        print('There was an exception here')
+    except TypeError as e:
+        print(e)  # You gave a not Homework object
+
     opp_teacher.check_homework(result_1)
-    temp_1 = opp_teacher.homework_done
+    print(Teacher.homework_done[oop_hw])
 
     advanced_python_teacher.check_homework(result_1)
-    temp_2 = Teacher.homework_done
-    assert temp_1 == temp_2
+    print(Teacher.homework_done[oop_hw])
 
     opp_teacher.check_homework(result_2)
     opp_teacher.check_homework(result_3)
 
     print(Teacher.homework_done[oop_hw])
-    Teacher.reset_results()
+
+    Teacher.reset_results(oop_hw)
+    print(Teacher.homework_done)
--- hw5/save_original_info.py
+++ hw5/save_original_info.py
@@ -19,8 +19,18 @@ print(custom_sum.__original_func)  # <function custom_sum at <some_id>>
 import functools
 
 
+def preserve_info(func):
+    def decorator_wrapper(wrapper_func):
+        wrapper_func.__doc__ = func.__doc__
+        wrapper_func.__name__ = func.__name__
+        wrapper_func.__original_func = func
+        return wrapper_func
+
+    return decorator_wrapper
+
+
 def print_result(func):
-    # Place for new decorator
+    @preserve_info(func)  # Добавляем декоратор на строку отведенную под него
     def wrapper(*args, **kwargs):
         """Function-wrapper which print result of an original function"""
         result = func(*args, **kwargs)
@@ -40,9 +50,7 @@ if __name__ == "__main__":
     custom_sum([1, 2, 3], [4, 5])
     custom_sum(1, 2, 3, 4)
 
-    print(custom_sum.__doc__)
-    print(custom_sum.__name__)
+    print(custom_sum.__doc__)  # 'This function can sum any objects which have __add___'
+    print(custom_sum.__name__)  # 'custom_sum'
     without_print = custom_sum.__original_func
-
-    # the result returns without printing
     without_print(1, 2, 3, 4)
